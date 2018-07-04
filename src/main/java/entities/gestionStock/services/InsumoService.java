23c774307eb2fc98fc382a6b7506187f512c5df6
jhon <juanpsenn@gmail.com>
2018-07-04 11:04:50 -0300

    service template added

---
+++ src/main/java/entities/gestionStock/services/InsumoService.java
@@ -0,0 +1,20 @@
+package entities.gestionStock.services;
+
+import javax.ws.rs.*;
+
+@Path("/stock")
+public class InsumoService {
+    @GET
+    @Produces("application/json")
+    public void get() {
+
+    }
+
+    @POST
+    @Consumes("application/json")
+    public void create() {
+
+    }
+
+
+}
