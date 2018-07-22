d39172bc854244dadf820a19a3ed68d33136cfec
diegoabanto1 <diegoabanto1@gmail.com>
2018-07-22 19:09:33 -0300

    Acoplando el Front-end

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -2,52 +2,83 @@
 app.controller("beFruitController", function ($scope, $http) {
 
     $scope.title = "HOLAAAAAAAAAA";
-    $scope.accion = 'Inicio'; 
-    $scope.subaccion = ''; 
-    $scope.unidadesmedida =[];
+    $scope.accion = 'Inicio';
 
-    //Accion...
-    
+
     $scope.Inicio = function () {
         $scope.accion = 'Inicio';
-       
+
     };
-    
+
     $scope.Venta = function () {
-        $scope.accion = 'Venta'; 
+        $scope.accion = 'Venta';
     };
-    
+
     $scope.Stock = function () {
         $scope.accion = 'Stock';
-        $scope.subaccion = 'Stock1';
-        $scope.insumos = $scope.obtenerInsumos()
+
     };
-    
+
     $scope.Caja = function () {
         $scope.accion = 'Caja';
     };
-    
+
     $scope.Empleados = function () {
-        $scope.accion = 'Empleados'; 
+        $scope.accion = 'Empleados';
     };
-    
+
     $scope.Informes = function () {
-        $scope.accion = 'Informes'; 
+        $scope.accion = 'Informes';
     };
-    
+
     $scope.Promociones = function () {
-        $scope.accion = 'Promociones'; 
+        $scope.accion = 'Promociones';
     };
-    
-    $scope.Sucursales = function () {
-        $scope.accion = 'Sucursales'; 
+
+    $scope.Franquicias = function () {
+        $scope.accion = 'Franquicias';
     };
-    
+
     $scope.Configuracion = function () {
-        $scope.accion = 'Configuracion'; 
+        $scope.accion = 'Configuracion';
     };
-    
+
+
+});
+
+app.controller("stockController", function ($scope, $http) {
+
+    $scope.subaccion = 'Stock1';
+
     //Subacciones...
+
+    $scope.Stock1 = function () {
+        $scope.subaccion = 'Stock1';
+
+    };
+
+    $scope.Stock2 = function () {
+        $scope.subaccion = 'Stock2';
+
+
+    };
+
+    $scope.Stock3 = function () {
+        $scope.subaccion = 'Stock3';
+        $scope.tempDetalles = [];
+    };
+
+
+    $scope.Stock4 = function () {
+        $scope.subaccion = 'Stock4';
+    };
+
+    $scope.Stock5 = function () {
+        $scope.subaccion = 'Stock5';
+    };
+
+    //Funciones...
+
     $scope.obtenerInsumos = function () {
         $http.get('/beFruit/stock/insumo')
             .then (function (response){
@@ -55,80 +86,104 @@ app.controller("beFruitController", function ($scope, $http) {
             });
     };
 
-    $scope.obtenerMovimientos = function() {
-        var datos = [{"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    {"idMovimientos":"1" ,"fecha":"10-08-2017","tipo":"Baja" ,"articulo":"Samasung galaxy s7" ,"cantidad":"50" },
-                    ];
-        return datos
+    $scope.obtenerMovimientos = function () {
+        $http.get('/beFruit/stock/movimientoStock')
+            .then(function (response) {
+                $scope.movimientos = response.data;
+            });
     };
 
 
 
-    $scope.obtenerUnidad = function () {
-        $http.get('/beFruit/stock/unidadMedida')
-            .then (function (response){
-                $scope.unidad = response.data;
+    $scope.anularMovimiento = function (m) {
+        if (confirm("¿Desea anular el movimiento " + m.id + "?") == true) {
+            $http.delete("/beFruit/stock/movimientoStock", m.id).then(
+                function (response) {
+                    alert("Movimiento anulado exitosamente");
+                    $scope.obtenerMovimientos();
+                },
+                function (response) {
+                    alert("No se puedo anular el movimiento...");
+                })
+        }
+        ;
+
+    };
+
+    $scope.verDetalles = function (m) {
+        $scope.detalles = m.detalles;
+    };
+
+
+    $scope.sumarDetalle = function (m, d) {
+        if ((d.insumo != null) && (d.cantidad != null) && (d.cantidad >= 1)) {
+            d.idMovimiento = m.id;
+            d.insumo = $scope.detalle.insumo;
+            $scope.tempDetalles.push(d);
+            $scope.detalle = {};
+        } else {
+            alert("Faltan datos...");
+        }
+        ;
+
+
+    };
+
+    $scope.restarDetalle = function (index) {
+        $scope.tempDetalles.splice(index, 1);
+
+    };
+
+    $scope.cancelarMovimiento = function () {
+        $scope.tempDetalles = [];
+        $scope.detalle = {};
+        $scope.movimiento = {};
+
+    };
+
+    $scope.agregarMovimiento = function () {
+        var count = 1;
+        var detalles = [];
+        angular.forEach($scope.tempDetalles, function (det) {
+            det.idMovimiento = $scope.movimiento.id;
+            det.idDetalleMovimientoStock = count;
+            detalles.push(det);
         });
-    }
+        $scope.CurrentDate = new Date();
+        var mov = {
+            "id": $scope.movimiento.id,
+            "idVenta": null,
+            "idTurno": null,
+            "fechaHora": $scope.CurrentDate,
+            "fechaHoraAnulacion": null,
+            "entrada": $scope.movimiento.tipo,
+            "detalles": detalles
+        };
+        $http.post("/beFruit/stock/movimientoStock", mov).then(function (response) {
+                $http.post("/beFruit/stock/detalleMovimientoStock", detalles, mov.id).then(function (response) {
+                    alert("Movimiento agregado");
 
-    $scope.unidadesmedida = $scope.obtenerUnidad();
+                });
+
+            },
+            function (response) {
+                alert("No se puedo agregar el movimiento...");
+            });
+        $scope.tempDetalles = [];
+        $scope.detalle = {};
+        $scope.movimiento = {};
 
 
-    //$scope.ObtenerUnMed = function ($scope, $http) {
-    //    $http.get('/unidadMedida')
-    //        .then(function (response) {
-    //            $scope.insumos = response.data; 
-    //};
-
-
-
-    $scope.Stock1 = function () { 
-        $scope.subaccion = 'Stock1'; 
-        
     };
 
-   
-    
-    $scope.Stock2 = function () { 
-        $scope.subaccion = 'Stock2'; 
-        $scope.movimientos = $scope.obtenerMovimientos ()
-        
-        
-    };
-    
-    $scope.Stock3 = function () { 
-        $scope.subaccion = 'Stock3'; 
-       
+    $scope.obtenerUnidades = function () {
+            $http.get('/beFruit/stock/unidadMedida')
+                .then (function (response){
+                    $scope.unidades = response.data;
+                });
         };
 
-        
-    
-    $scope.Stock4 = function () { 
-        $scope.subaccion = 'Stock4';
-        
-        //$scope.ObtenerUnMed();
-        
-        $scope.unidadesmedida = $scope.ListaUnMed();
-    };
-    $scope.nvoInsumo = $scope.nvoInsumo;
-    
-    $scope.Grabar = function () {
+    $scope.grabarInsumo = function () {
         if ($scope.nvoInsumo.idInsumo == undefined)  // agregar
         {
             $http.post('/application/json', $scope.nvoInsumo).then(function (response) {
@@ -147,34 +202,70 @@ app.controller("beFruitController", function ($scope, $http) {
         }
     };
 
-    $scope.Agregar = function () {
-        $scope.nvoInsumo = {};
-        $scope.Stock4();
-    };
-    $scope.Borrar = function () {
-     
+    $scope.borrarInsumo = function () {
+
         $http.put('/' + $scope.nvoInsumo.idInsumo, $scope.nvoInsumotoJson).then(function (response) {
-            
-                alert("Registro eliminado")
-            });
-        
+
+            alert("Registro eliminado")
+        });
+
     };
 
- 
-
-    $scope.BuscarPorId = function (Insumo) {
+    $scope.buscarInsumoPorId = function (Insumo) {
         $scope.nvoInsumo = Insumo;
         $scope.Stock4();
     };
 
-    $scope.BuscarUmPorId = function (unidadMedida) {
+    $scope.buscarUnidadMedidaPorId = function (unidadMedida) {
         $scope.nvoUnidad = unidadMedida;
     };
-    
-    $scope.Stock5 = function () { 
-        $scope.subaccion = 'Stock5';
-        $scope.unidadesMedida = $scope.obtenerUnidadMedida
-    };
-    
 
+
+    //Valores inicializados (sino materialize no carga los select):
+
+    $scope.insumos = $scope.obtenerInsumos();
+    $scope.unidades = $scope.obtenerUnidades();
+    $scope.movimientos = $scope.obtenerMovimientos();
+    $scope.nvoInsumo = $scope.nvoInsumo;
+
+
+});
+
+
+app.controller("franquiciasController", function ($scope, $http) {
+
+    $scope.subaccion = 'Franquicias1';
+
+    //Subacciones...
+
+    $scope.Franquicias1 = function () {
+        $scope.subaccion = 'Franquicias1';
+
+    };
+
+    $scope.Franquicias2 = function () {
+        $scope.subaccion = 'Franquicias2';
+
+    };
+
+    //Funciones...
+
+});
+
+app.controller("ventaController", function ($scope, $http) {
+});
+
+app.controller("cajaController", function ($scope, $http) {
+});
+
+app.controller("empleadosController", function ($scope, $http) {
+});
+
+app.controller("informesController", function ($scope, $http) {
+});
+
+app.controller("promocionesController", function ($scope, $http) {
+});
+
+app.controller("configController", function ($scope, $http) {
 });
